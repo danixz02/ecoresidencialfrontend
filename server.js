197abe70e696b0f99002ac8014071ec6855baf62
danixz02 <danielmobile098@gmail.com>
2025-06-16 20:56:57 -0300

    ajuste no CROS

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(MONGODB_URI, {
 
 // ==== Middlewares ====
 app.use(cors({
-  origin: ['https://ecoresidencialapi.onrender.com'/* , 'https://ecoresidencial.vercel.app' */],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
